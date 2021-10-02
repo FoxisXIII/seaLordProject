19fa6a13c01a6e8a00e6f5c0fc63488bdefea40e
eespunes <erik.espunyes7@outlook.com>
2021-10-02 10:31:06 +0200

    Created the initial project

---
+++ Source/seaLordProject/seaLordProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "seaLordProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEALORDPROJECT_API AseaLordProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
